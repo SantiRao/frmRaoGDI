d9208ff5b0adee212dfcbc5c13ba4d2551538e00
root <mgiraudo@ies21.edu.ar>
2023-08-31 13:18:14 -0300

    cambios

--- frmArkanoidLVL2.Designer.cs
+++ frmArkanoidLVL2.Designer.cs
@@ -35,6 +35,8 @@
             this.picNave = new System.Windows.Forms.PictureBox();
             this.Contenedor = new System.Windows.Forms.PictureBox();
             this.btnEmpezar = new System.Windows.Forms.Button();
+            this.btnDerecha = new System.Windows.Forms.Button();
+            this.btnIzquierda = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picPelota)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picNave)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Contenedor)).BeginInit();
@@ -42,9 +44,10 @@
             // 
             // picPelota
             // 
+            this.picPelota.BackColor = System.Drawing.Color.Transparent;
             this.picPelota.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picPelota.BackgroundImage")));
             this.picPelota.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.picPelota.Location = new System.Drawing.Point(283, 329);
+            this.picPelota.Location = new System.Drawing.Point(281, 264);
             this.picPelota.Name = "picPelota";
             this.picPelota.Size = new System.Drawing.Size(29, 29);
             this.picPelota.TabIndex = 7;
@@ -53,7 +56,7 @@
             // picNave
             // 
             this.picNave.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.picNave.Location = new System.Drawing.Point(248, 381);
+            this.picNave.Location = new System.Drawing.Point(246, 299);
             this.picNave.Name = "picNave";
             this.picNave.Size = new System.Drawing.Size(100, 33);
             this.picNave.TabIndex = 6;
@@ -61,6 +64,7 @@
             // 
             // Contenedor
             // 
+            this.Contenedor.BackColor = System.Drawing.Color.Lime;
             this.Contenedor.Location = new System.Drawing.Point(0, 1);
             this.Contenedor.Name = "Contenedor";
             this.Contenedor.Size = new System.Drawing.Size(612, 456);
@@ -77,17 +81,43 @@
             this.btnEmpezar.UseVisualStyleBackColor = true;
             this.btnEmpezar.Click += new System.EventHandler(this.btnEmpezar_Click);
             // 
+            // btnDerecha
+            // 
+            this.btnDerecha.Enabled = false;
+            this.btnDerecha.Location = new System.Drawing.Point(298, 393);
+            this.btnDerecha.Name = "btnDerecha";
+            this.btnDerecha.Size = new System.Drawing.Size(75, 23);
+            this.btnDerecha.TabIndex = 9;
+            this.btnDerecha.Text = ">>";
+            this.btnDerecha.UseVisualStyleBackColor = true;
+            this.btnDerecha.Click += new System.EventHandler(this.btnDerecha_Click);
+            // 
+            // btnIzquierda
+            // 
+            this.btnIzquierda.Enabled = false;
+            this.btnIzquierda.Location = new System.Drawing.Point(217, 393);
+            this.btnIzquierda.Name = "btnIzquierda";
+            this.btnIzquierda.Size = new System.Drawing.Size(75, 23);
+            this.btnIzquierda.TabIndex = 11;
+            this.btnIzquierda.Text = "<<";
+            this.btnIzquierda.UseVisualStyleBackColor = true;
+            this.btnIzquierda.Click += new System.EventHandler(this.btnIzquierda_Click);
+            // 
             // frmArkanoidLVL2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(616, 450);
+            this.Controls.Add(this.btnIzquierda);
+            this.Controls.Add(this.btnDerecha);
             this.Controls.Add(this.picPelota);
             this.Controls.Add(this.picNave);
             this.Controls.Add(this.btnEmpezar);
             this.Controls.Add(this.Contenedor);
+            this.KeyPreview = true;
             this.Name = "frmArkanoidLVL2";
             this.Text = "frmArkanoidLVL2";
+            this.Load += new System.EventHandler(this.frmArkanoidLVL2_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmArkanoidLVL2_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.picPelota)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picNave)).EndInit();
@@ -103,5 +133,7 @@
         private System.Windows.Forms.PictureBox picNave;
         private System.Windows.Forms.PictureBox Contenedor;
         private System.Windows.Forms.Button btnEmpezar;
+        private System.Windows.Forms.Button btnDerecha;
+        private System.Windows.Forms.Button btnIzquierda;
     }
 }
--- frmArkanoidLVL2.cs
+++ frmArkanoidLVL2.cs
@@ -17,24 +17,46 @@ namespace frmRaoGDI
             InitializeComponent();
         }
 
-        private void btnEmpezar_Click(object sender, KeyEventArgs e)
-        {
-        }
-
         private void frmArkanoidLVL2_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left)
-            {
-                MessageBox.Show("presiono izquierda");
-            }
+                
         }
 
         private void btnEmpezar_Click(object sender, EventArgs e)
         {
+            picPelota.Show();
+            btnDerecha.Enabled = true;
+            btnIzquierda.Enabled = true;
             // enciende el reloj
             Reloj.Enabled = true;
             btnEmpezar.Hide();
+            EjeY = picPelota.Location.Y;
             this.Focus();
+        }
+
+        private void frmArkanoidLVL2_Load(object sender, EventArgs e)
+        {
+            picPelota.Hide();
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.BackColor = Color.Red;
+        }
+
+        int EjeY = 0;
+        int EjeX = 0;   
+
+        private void btnIzquierda_Click(object sender, EventArgs e)
+        {
+            EjeX = picNave.Location.X;
+
+            picNave.Location = new Point(EjeX - 25, picNave.Location.Y);
+        }
+
+        private void btnDerecha_Click(object sender, EventArgs e)
+        { 
+
+            EjeX = picNave.Location.X;
+
+            picNave.Location = new Point(EjeX + 25, picNave.Location.Y);
         }
     }
 }
